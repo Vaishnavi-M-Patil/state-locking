87084cfd4c5c955e95eb1e217159a3a5eb884eba
Vaishnavi <vaishnavipatil6002@gmail.com>
2025-05-16 16:29:24 +0530

    2 projects are created

---
+++ project2/main.tf
@@ -0,0 +1,21 @@
+terraform {
+    backend "s3" {
+        bucket = "terraform-backend-1283"
+        region = "us-east-1"
+        key = "backend/terraform.tfstate"     
+        dynamodb_table = "state_lock" 
+    }
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "ec2-instance" {
+    ami = "ami-0953476d60561c955"
+    instance_type = "t2.micro"
+    key_name = "vir-key"
+    tags = {
+      Name = "ec2 instance- developer2"
+    }
+}
